42879df0904ebf7f572fab464737f1dd6786b329
rafalangoni <39811730+rafalangoni@users.noreply.github.com>
2022-06-28 20:14:08 -0300

    melhorias css

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,15 +3,16 @@ const botaoSubmit = document.querySelector("#botao__submit");
 let resultadoRating = document.querySelector(".resultado__rating");
 let notaDoRestaurante = 0;
 
-itensAvaliacao.forEach(elemento => {
+itensAvaliacao.forEach((elemento, index) => {
     elemento.addEventListener("click", () => {
+        elemento.classList.add('elemento__selecionado');
         notaDoRestaurante = elemento.innerHTML;
     })
     return notaDoRestaurante;
 })
 
 botaoSubmit.addEventListener("click", () => {
-    if(notaDoRestaurante === 0){
+    if (notaDoRestaurante === 0) {
         alert('Please rate our restaurant!');
         return;
     }
